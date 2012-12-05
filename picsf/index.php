f9555f9d24fe3eebb194b456ea1468d4c6d9e040
IDEA ESB <ideaesb@gmail.com>
2012-12-05 13:11:56 -1000

    PICSF - init

---
+++ picsf/index.php
@@ -0,0 +1,45 @@
+<?php
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+?> 
+<html>
+<head>
+<title>Pacific Islands Climate Services Forum January 21-25 2013</title>
+</head>
+
+<body>
+
+<p class=MsoBodyText align=center style='text-align:center'><img src="logos.png"></p>
+
+
+<p class=MsoBodyText align=center style='text-align:center'><b><span
+style='font-size:14.0pt;font-family:"Cambria","serif"'>Pacific Islands Climate Services Forum</span></b></p>
+
+<p class=MsoBodyText align=center style='text-align:center'><b><span
+style='font-size:12.0pt;font-family:"Cambria","serif"'>January 21-25, 2013</span></b></p>
+
+<p class=MsoBodyText align=center style='text-align:center'><span
+style='font-size:14.0pt;font-family:"Cambria","serif"'>University of the South Pacific - Laucala Campus</p>
+
+<p class=MsoBodyText align=center style='text-align:center'><span style='font-size:14.0pt;font-family:"Cambria","serif"'>Suva, Fiji</span></p>
+
+<p class=MsoBodyText><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'>&nbsp;</span></b></p>
+
+<p class=MsoBodyText align=center style='text-align:center'><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'><a href="../docs/Updated%20Announcement%20v4.pdf" target="_blank">Announcement - Forum</a></span></b></p>
+<p class=MsoBodyText><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'>&nbsp;</span></b></p>
+<p class=MsoBodyText align=center style='text-align:center'><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'><a href="../docs/PICSF%20Partners%20Meeting%20Announcement.pdf" target="_blank">Announcement - Partners Meeting</a></span></b></p>
+<p class=MsoBodyText><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'>&nbsp;</span></b></p>
+<p class=MsoBodyText align=center style='text-align:center'><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'><a href="#" target="_blank">Participants</a></span></b></p>
+<p class=MsoBodyText><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'>&nbsp;</span></b></p>
+<p class=MsoBodyText align=center style='text-align:center'><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'><a href="agenda.pdf" target="_blank">Agenda</a></span></b></p>
+<p class=MsoBodyText><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'>&nbsp;</span></b></p>
+<p class=MsoBodyText align=center style='text-align:center'><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'><a href="lodging.pdf" target="_blank">Lodging</a></span></b></p>
+<p class=MsoBodyText><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'>&nbsp;</span></b></p>
+<p class=MsoBodyText align=center style='text-align:center'><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'><a href="poster.pdf" target="_blank">Poster Session</a> (Deadline: 15 December 2012 5 PM EST)</span></b></p>
+<p class=MsoBodyText><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'>&nbsp;</span></b></p>
+<p class=MsoBodyText align=center style='text-align:center'><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'><a href="https://docs.google.com/spreadsheet/viewform?formkey=dDRJY0RLNDYwamRTNHNvWjVGWlNYOUE6MQ" target="_blank">RSVP</a>/<a href="https://docs.google.com/spreadsheet/viewform?formkey=dHhTQ0gxNXBjOE5WbjVCSUdKaXBySXc6MQ" target="_blank">Registration</a> (Deadline: 24 December 2012 Noon EST)</span></b></p>
+
+
+</body>
+
+</html>
