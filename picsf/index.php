b77ee80ffba3fd5e50589c181a48c352128e1cc6
IDEA ESB <ideaesb@gmail.com>
2012-12-05 16:39:15 -1000

    Britt's comments - received - Wed, Dec 5, 2012 at 1:59 PM HST

--- picsf/index.php
+++ picsf/index.php
@@ -19,25 +19,28 @@ style='font-size:14.0pt;font-family:"Cambria","serif"'>Pacific Islands Climate S
 style='font-size:12.0pt;font-family:"Cambria","serif"'>January 21-25, 2013</span></b></p>
 
 <p class=MsoBodyText align=center style='text-align:center'><span
-style='font-size:14.0pt;font-family:"Cambria","serif"'>University of the South Pacific - Laucala Campus</p>
+style='font-size:14.0pt;font-family:"Cambria","serif"'>University of the South Pacific - Laucala Campus<br />Suva, Fiji</span></p>
 
-<p class=MsoBodyText align=center style='text-align:center'><span style='font-size:14.0pt;font-family:"Cambria","serif"'>Suva, Fiji</span></p>
+<div style="margin-left:20%;margin-right:20%">
+<p class=MsoBodyText><span style='margin: 5%;font-size:12.0pt;font-family:"Cambria","serif"'>The Pacific Islands Climate Services Forum is hosted by the Pacific Centre for Environment and Sustainable Development (<a href="http://www.usp.ac.fj/index.php?id=570/" title="PACE-SD" target="_blank">PACE-SD</a>) and the Pacific Climate Information System (<a href="http://www.climate.gov/pacific" title="Climate.gov - Pacific Region" target="_blank">PaCIS</a>).  The forum is aimed at engaging climate experts in a dialogue with resource and disaster risk managers, community planners and other decision makers, the EU GCCA in country coordinators, meteorological service representatives, postgraduate students, and the greater climate services community, as well as representatives of government ministries and other program managers and policy makers.</span></p>
+</div>
 
 <p class=MsoBodyText><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'>&nbsp;</span></b></p>
-
-<p class=MsoBodyText align=center style='text-align:center'><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'><a href="../docs/Updated%20Announcement%20v4.pdf" target="_blank">Announcement - Forum</a></span></b></p>
+<p class=MsoBodyText align=center style='text-align:center'><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'><a href="../docs/Updated%20Announcement%20v4.pdf" target="_blank">Announcement</a></span></b></p>
+<!--
 <p class=MsoBodyText><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'>&nbsp;</span></b></p>
 <p class=MsoBodyText align=center style='text-align:center'><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'><a href="../docs/PICSF%20Partners%20Meeting%20Announcement.pdf" target="_blank">Announcement - Partners Meeting</a></span></b></p>
+-->
 <p class=MsoBodyText><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'>&nbsp;</span></b></p>
 <p class=MsoBodyText align=center style='text-align:center'><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'><a href="#" target="_blank">Participants</a></span></b></p>
 <p class=MsoBodyText><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'>&nbsp;</span></b></p>
-<p class=MsoBodyText align=center style='text-align:center'><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'><a href="agenda.pdf" target="_blank">Agenda</a></span></b></p>
+<p class=MsoBodyText align=center style='text-align:center'><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'><a href="agenda.pdf" target="_blank">Agenda - Week-at-a-Glance (Draft)</a></span></b></p>
 <p class=MsoBodyText><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'>&nbsp;</span></b></p>
 <p class=MsoBodyText align=center style='text-align:center'><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'><a href="lodging.pdf" target="_blank">Lodging</a></span></b></p>
 <p class=MsoBodyText><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'>&nbsp;</span></b></p>
 <p class=MsoBodyText align=center style='text-align:center'><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'><a href="poster.pdf" target="_blank">Poster Session</a> (Deadline: 15 December 2012 5 PM EST)</span></b></p>
 <p class=MsoBodyText><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'>&nbsp;</span></b></p>
-<p class=MsoBodyText align=center style='text-align:center'><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'><a href="https://docs.google.com/spreadsheet/viewform?formkey=dDRJY0RLNDYwamRTNHNvWjVGWlNYOUE6MQ" target="_blank">RSVP</a>/<a href="https://docs.google.com/spreadsheet/viewform?formkey=dHhTQ0gxNXBjOE5WbjVCSUdKaXBySXc6MQ" target="_blank">Registration</a> (Deadline: 24 December 2012 Noon EST)</span></b></p>
+<p class=MsoBodyText align=center style='text-align:center'><b><span style='font-size:12.0pt;font-family:"Cambria","serif"'>Please, FIRST, <a href="https://docs.google.com/spreadsheet/viewform?formkey=dDRJY0RLNDYwamRTNHNvWjVGWlNYOUE6MQ" target="_blank">RSVP</a> and, THEN,  <a href="https://docs.google.com/spreadsheet/viewform?formkey=dHhTQ0gxNXBjOE5WbjVCSUdKaXBySXc6MQ" target="_blank">register</a>. (Deadline: 24 December 2012 Noon EST)</span></b></p>
 
 
 </body>
